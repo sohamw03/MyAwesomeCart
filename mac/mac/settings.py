fbd9faa194d8b7a70857bcad9ded8958440b6fb8
Soham Waghmare <102748742+sohamw03@users.noreply.github.com>
2025-03-15 05:03:25 +0530

    fix: chore

--- mac/mac/settings.py
+++ mac/mac/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["my-awesome-cart-theta.vercel.app", ]
+ALLOWED_HOSTS = ["my-awesome-cart-theta.vercel.app", "*.vercel.app"]
 
 
 # Application definition
